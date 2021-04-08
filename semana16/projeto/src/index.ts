b2c9687944a86a816b8c547c272cc9132d46f96d
Letícia Chijo <leticia.chijo@gmail.com>
2021-04-08 00:19:18 -0300

    Setup do Projeto

--- semana16/projeto/src/index.ts
+++ semana16/projeto/src/index.ts
@@ -1,34 +1,3 @@
-# Passos para começar um projeto
-
-1) Criar **package.json**: `npm init -y`
-2) Criar **tsconfig.json**: `tsc --init`
-
-```
-{
-  "compilerOptions": {
-      "target": "es6",
-      "module": "commonjs",
-      "sourceMap": true,
-      "outDir": "./build",
-      "rootDir": "./src",
-      "removeComments": true,
-      "noImplicitAny": true,
-      "esModuleInterop": true
-  }
-}
-```
-
- 4) Criar pasta **src**
- 5) Criar comando **npm start**: `"start": "tsc && node ./build/index.js"`
- 6) Criar **gitignore**: `touch .gitignore` e adicionar node_modules, build e .env
- 7) Instalar **express** e tipos do express: `npm i express` e `npm i @types/express -D`
- 8) Instalar **cors** e tipos do cors: `npm i cors` e `npm i @types/cors -D`
- 9) Instalar knex, mysql e dotenv: `npm i knex@0.21.15 mysql dotenv`
- 10) Instalar tipos knex: `npm i @types/knex -D`
- 11) Criar o arquivo `.env` com as variáveis de ambiente
- 12) Criar arquivo `index.ts` com o seguinte conteúdo
-
-```
 import express, { Express, Request, Response } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
@@ -60,7 +29,6 @@ const connection = knex({
 // --- ENDPOINTS --------------------------------------------------------------
 
 // --- INICIAR SERVER ---------------------------------------------------------
-
 const server = app.listen(PORT || 3003, () => {
     if (server) {
         const address = server.address() as AddressInfo
@@ -69,4 +37,3 @@ const server = app.listen(PORT || 3003, () => {
         console.error(`Failure upon starting server.`)
     }
 })
-```
